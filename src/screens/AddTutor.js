760c6213ada1bffa944d0e0a8f05290d03f33ee5
rheo-chiti <rheo-chiti>
2020-11-20 15:35:51 +0530

    added Tutor screen

--- src/screens/AddTutor.js
+++ src/screens/AddTutor.js
@@ -1,11 +1,120 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Typography, Paper, Avatar, Button, FormControl, Input, InputLabel } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import withStyles from '@material-ui/core/styles/withStyles'
+import { Link, withRouter } from 'react-router-dom'
+import Select from 'react-select'
+import countryList from 'react-select-country-list'
+import firebase from "firebase";
+import 'firebase/firebase-firestore'
+import app from 'firebase/app'
 
-function AddTutor() {
+const styles = theme => ({
+    main: {
+        width: 'auto',
+        display: 'block', // Fix IE 11 issue.
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    avatar: {
+        margin: theme.spacing.unit,
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing.unit,
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+    },
+})
+
+function AddTutor(props) {
+    const { classes } = props
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [country, setCountry] = useState('')
+
+    const options = countryList().getData()
+
+    const changeHandler = value => {
+        setCountry(value)
+    }
+
+
+
     return (
-        <div>
-            <p>Add a new Tutor</p>
-        </div>
+        <main className={classes.main}>
+            <Paper className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Add Tutor
+       			</Typography>
+                <form className={classes.form} onSubmit={e => e.preventDefault() && false}>
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="name">Name</InputLabel>
+                        <Input id="name" name="name" autoComplete="off" autoFocus value={name} onChange={e => setName(e.target.value)} />
+                    </FormControl>
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="email">Email Address</InputLabel>
+                        <Input id="email" name="email" autoComplete="off" value={email} onChange={e => setEmail(e.target.value)} />
+                    </FormControl>
+                    <FormControl margin="normal" required fullWidth>
+                        <Select
+                            options={options}
+                            value={country}
+                            onChange={changeHandler}
+                        />
+                    </FormControl>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        onClick={onRegister}
+                        // component={Link}
+                        // to="/register"
+                        className={classes.submit}>
+                        Register
+          			</Button>
+                </form>
+            </Paper>
+        </main>
     )
+
+
+    async function onRegister() {
+        if (country == '') {
+            alert("Please Fill All Required Field");
+            return;
+        }
+        console.log(name, email, country.label)
+        try {
+            const db = app.firestore()
+            db.collection("tutors").add({
+                name: name,
+                email: email,
+                country: country.label
+            })
+        } catch (error) {
+            alert(error.message)
+        }
+    }
 }
 
-export default AddTutor
+export default withStyles(styles)(AddTutor)
--- src/screens/ViewTutor.js
+++ src/screens/ViewTutor.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import {useState} from 'react'
+import {useEffect} from 'react'
+import app from 'firebase/app'
+import 'firebase/firebase-firestore'
 
-function ViewTutor() {
+export default function ViewTutor() {
+    const [tutors, setTutors] = useState();
+
+    useEffect(() => {
+        const db = app.firestore();
+        return db.collection('tutors').onSnapshot((snapshot) => {
+            const data = [];
+            snapshot.forEach((doc) => data.push({ ...doc.data(), id: doc.id }));
+            console.log(data);  // <------
+            setTutors(data);
+        });
+    }, []);
+
+    console.log(tutors)
+
     return (
         <div>
-            <p>View Tutor</p>
+            <p>aa</p>
         </div>
     )
 }
-
-export default ViewTutor
