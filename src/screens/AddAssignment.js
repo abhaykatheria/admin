d48d6f9e842100c543aa6787d8f8873ec0572991
rheo-chiti <rheo-chiti>
2020-11-29 20:27:15 +0530

    added dues & payment collection logic

--- src/screens/AddAssignment.js
+++ src/screens/AddAssignment.js
@@ -64,6 +64,8 @@ function AddAssignment(props) {
     const [file, setFile] = useState('')
     const [tutorId, setTutorId] = useState('')
     const [dues, setDues] = useState(0)
+    const [assId, setAssId] = useState('')
+
 
     const changeHandler = value => {
         setTutor(value)
@@ -72,21 +74,29 @@ function AddAssignment(props) {
 
     useEffect(() => {
         const db = app.firestore();
-        window.scrollTo(0,0);
+        window.scrollTo(0, 0);
         return db.collection('tutors').onSnapshot((snapshot) => {
             const data = [];
-            snapshot.forEach((doc) => data.push({ ...doc.data() }));
+            const ids = [];
+            snapshot.forEach((doc) => {
+                data.push({ ...doc.data() })
+                ids.push(doc.id)
+                // console.log(doc.id);
+            }
+            );
+            console.log(ids);
             let data1 = []
             for (var i = 0; i < data.length; i++) {
-                data1.push(data[i].name);
-                // console.log(data[i]).name);
+                data1.push({ name: data[i].name, id: ids[i] });
+                // console.log(data[i]);
             }
             data1 = data1.map((dat) => {
                 return {
-                    value: dat,
-                    label: dat
+                    value: dat.id,
+                    label: dat.name
                 }
             })
+            // console.log(data1);
             setAllTutors(data1);
         });
     }, []);
@@ -96,83 +106,83 @@ function AddAssignment(props) {
     const onChange = (e) => {
         const file = e.target.files[0];
         setFile(file);
-        console.log(file)
+        // console.log(file)
     }
 
 
 
     return (
-        <div style = {{backgroundColor: "#dee4e3"}}>
-        <main className={classes.main}>
-            <Paper className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Add Assignment
+        <div style={{ backgroundColor: "#dee4e3" }}>
+            <main className={classes.main}>
+                <Paper className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Add Assignment
        			</Typography>
-                <form className={classes.form} onSubmit={e => e.preventDefault() && false}>
-                    <FormControl margin="normal" required fullWidth>
-                        <InputLabel htmlFor="name">Student's name</InputLabel>
-                        <Input id="name" name="name" autoComplete="off" autoFocus value={student} onChange={e => setstudent(e.target.value)} />
-                    </FormControl>
-                    <FormControl margin="normal" required fullWidth>
-                        <p>Tutor</p>
-                        <Select
-                            options={allTutors}
-                            value={tutor}
-                            onChange={changeHandler}
-                        />
-                    </FormControl>
-                    <FormControl margin="normal" required fullWidth>
-                        <InputLabel htmlFor="name">Subject</InputLabel>
-                        <Input id="name" name="name" autoComplete="off" autoFocus value={subject} onChange={e => setSubject(e.target.value)} />
-                    </FormControl>
-                    <FormControl margin="normal" required fullWidth>
-                        <InputLabel htmlFor="name">Price</InputLabel>
-                        <Input id="name" name="name" autoComplete="off" autoFocus value={price} onChange={e => setPrice(e.target.value)} />
-                    </FormControl>
-                    <FormControl margin="normal" required fullWidth>
-                        <InputLabel htmlFor="name">Amount Paid</InputLabel>
-                        <Input id="name" name="name" autoComplete="off" autoFocus value={amount_paid} onChange={e => setAmountPaid(e.target.value)} />
-                    </FormControl>
-                    <FormControl margin="normal" required fullWidth>
-                        <InputLabel htmlFor="name">Tutor Fee</InputLabel>
-                        <Input id="name" name="name" autoComplete="off" autoFocus value={tutor_fee} onChange={e => setTutorFee(e.target.value)} />
-                    </FormControl>
-                    <FormControl margin="normal" required fullWidth>
-                        <InputLabel htmlFor="name">Comments</InputLabel>
-                        <Input id="name" name="name" autoComplete="off" autoFocus value={comments} onChange={e => setComments(e.target.value)} />
-                    </FormControl>
-                    <FormControl margin="normal" required fullWidth>
-                        <p>Due Date</p>
-                        <DateTimePicker
-                            onChange={setDueDate}
-                            value={due_date}
-                        />
-                    </FormControl>
-                    <Button
-                        variant="contained"
-                        component="label"
-                    >
-                        Upload File
+                    <form className={classes.form} onSubmit={e => e.preventDefault() && false}>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="name">Student's name</InputLabel>
+                            <Input id="name" name="name" autoComplete="off" autoFocus value={student} onChange={e => setstudent(e.target.value)} />
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <p>Tutor</p>
+                            <Select
+                                options={allTutors}
+                                value={tutor}
+                                onChange={changeHandler}
+                            />
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="name">Subject</InputLabel>
+                            <Input id="name" name="name" autoComplete="off" autoFocus value={subject} onChange={e => setSubject(e.target.value)} />
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="name">Price</InputLabel>
+                            <Input id="name" name="name" autoComplete="off" autoFocus value={price} onChange={e => setPrice(e.target.value)} />
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="name">Amount Paid</InputLabel>
+                            <Input id="name" name="name" autoComplete="off" autoFocus value={amount_paid} onChange={e => setAmountPaid(e.target.value)} />
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="name">Tutor Fee</InputLabel>
+                            <Input id="name" name="name" autoComplete="off" autoFocus value={tutor_fee} onChange={e => setTutorFee(e.target.value)} />
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="name">Comments</InputLabel>
+                            <Input id="name" name="name" autoComplete="off" autoFocus value={comments} onChange={e => setComments(e.target.value)} />
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <p>Due Date</p>
+                            <DateTimePicker
+                                onChange={setDueDate}
+                                value={due_date}
+                            />
+                        </FormControl>
+                        <Button
+                            variant="contained"
+                            component="label"
+                        >
+                            Upload File
                         <input type="file" hidden onChange={onChange} />
-                    </Button>
-                    {file && <p>{file.name}</p>}
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        onClick={onRegister}
-                        // component={Link}
-                        // to="/register"
-                        className={classes.submit}>
-                        Register
+                        </Button>
+                        {file && <p>{file.name}</p>}
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            onClick={onRegister}
+                            // component={Link}
+                            // to="/register"
+                            className={classes.submit}>
+                            Register
           			</Button>
-                </form>
-            </Paper>
-        </main>
+                    </form>
+                </Paper>
+            </main>
         </div>
     )
 
@@ -184,7 +194,7 @@ function AddAssignment(props) {
         const url = await fileRef.getDownloadURL().catch((error) => { throw error });
         if (url != '') {
             setAssURL(url)
-            console.log(url);
+            // console.log(url);
         }
     }
 
@@ -194,7 +204,16 @@ function AddAssignment(props) {
             alert("Please Fill All Required Field");
             return;
         }
-        console.log(student, tutor.value, subject, price, amount_paid, tutor_fee, comments);
+        console.log(student, tutor.label, subject, price, amount_paid, tutor_fee, comments);
+        console.log(allTutors);
+        for (var i = 0; i < allTutors.length; i++) {
+            if (allTutors[i].label == tutor.label) {
+                setTutorId(allTutors[i].value)
+                console.log(tutorId);
+                break
+            }
+        }
+
         // console.log(name, email, country.label)
         try {
             upload();
@@ -211,20 +230,24 @@ function AddAssignment(props) {
                         satus: satus,
                         student: student,
                         subject: subject,
-                        tutor: tutor.value,
+                        tutor: tutor.label,
                         tutor_fee: parseInt(tutor_fee),
                         assURL: assURL
+                    }).then((doc) => {
+                        console.log(doc.id, due_date, price - amount_paid, student);
+                        console.log(doc.id, due_date, "pending", tutor.label, tutor_fee, tutorId);
+                        setAssId(doc.id)
                     })
 
                     db.collection('tutors').onSnapshot((snapshot) => {
                         snapshot.forEach((doc) => {
-                            if (doc.data().name === tutor.value) {
+                            if (doc.data().name === tutor.label) {
                                 var temp = doc.data()
                                 temp.dues += parseInt(tutor_fee)
                                 setTutorId(doc.id)
                                 setDues(temp.dues)
                             }
-                            console.log(doc.id, doc.data(), tutor.value);
+                            // console.log(doc.id, doc.data(), tutor.value);
                         });
                     })
                 } catch (error) {
@@ -235,21 +258,58 @@ function AddAssignment(props) {
             alert(error.message)
         }
         updateDues()
+        updateDuesCollection()
+        updatePayment()
     }
 
-    function updateDues() {
-        try{
-            if(tutorId != ''){
-                console.log(dues);
-                app.firestore().collection('tutors').doc(tutorId).update({
+    async function updateDues() {
+        try {
+            if (tutorId != '') {
+                // console.log(dues);
+                await app.firestore().collection('tutors').doc(tutorId).update({
                     dues: dues
                 })
+
             }
         }
-        catch(error){
+        catch (error) {
             alert(error.message)
         }
     }
+
+    async function updateDuesCollection() {
+        try {
+            // console.log(dues);
+            await app.firestore().collection('dues').add({
+                assg_id: assId,
+                due_date: due_date,
+                status: "pending",
+                tutor: tutor.label,
+                tutor_fee: tutor_fee,
+                tutorId: tutorId
+            })
+        }
+        catch (error) {
+            alert(error.message)
+        }
+    }
+
+    async function updatePayment() {
+        try {
+            // console.log(dues);
+            await app.firestore().collection('payment_collection').add({
+                assg_id: assId,
+                due_date: due_date,
+                pending: price-amount_paid,
+                status: "pending",
+                student: student
+            })
+        }
+        catch (error) {
+            alert(error.message)
+        }
+    }
+
 
 }
 
